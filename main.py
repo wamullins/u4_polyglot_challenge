915fc533989831b90a2a789b139c1084682dbfdc
jataubman <jeremy.taubman@generalassemb.ly>
2022-08-27 11:43:41 -0400

    adding images

--- main.py
+++ main.py
@@ -26,3 +26,22 @@
 
 
 # Solution Goes Here - >
+
+
+
+
+# Challenge: 03 is_palindrome
+
+# Prompt:
+# - Write a function called is_palindrome that accepts a single string argument, then returns true or false depending upon whether or not the string is a palindrome.
+# - A palindrome is a word or phrase that is the same forward or backward.
+# - Casing and spaces are not included when considering whether or not a string is a palindrome.
+# - If the length of the string is 0 or 1, return true.
+# Examples:
+# is_palindrome('SEI Rocks'); //=> false
+# is_palindrome('rotor'); //=> true
+# is_palindrome('A nut for a jar of tuna'); //=> true
+# is_palindrome(''); //=> true
+
+
+# Solution Goes Here - >
