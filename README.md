b76b76f4b801c087e2a2f72cb8973cec9e14b540
jataubman <jeremy.taubman@generalassemb.ly>
2022-08-27 11:59:41 -0400

    more editing

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # SEIR 621 Polyglot Challenge
 
-As you continue on your journey studying and working with code, it is essential for you to take your existing knowledge and build up on your skills on new things.
 
-Remember, the languages and frameworks that we use are constantly changing, and if you are not able to build up on your existing skills into something new, you will be doing yourself a disservice when you get into the job application process.
+![JonStewart](./JS_HB.jpg)
+
+
+As you continue on your journey studying and working with code, it is critical for you to take your existing skills and build up on your ability to learn new things. Remember, the languages and frameworks that we use are constantly changing - building up your skills and adapting to change is critical for success in this field.
 
 For this activity, lets take a few code challenges that we are familiar with in Javascript, and try them out in our new language, Python
 
-You should already know the steps required for these challenges, and the different parts involved (Looping, conditionals, mathematic operators, string concatination...). What is going to be most important in this is the ability to translate your already existing skills into a new language. Because if you can translate your basic JS to Python, you will be able to use these same skills to learn Ruby, Java, Rust, or any of the other many, many languages out there that you will see in the wild.
+You should already know the steps required for these challenges, and the different parts involved (Looping, conditionals, mathematic operators, string concatination...). What is going to be most important in this is the ability to translate your already existing skills into a new language. 
+
+Because if you can translate your basic JS to Python, you will be able to use these same skills to learn Ruby, Java, Rust, or any of the other many, many languages out there that you will see in the wild.
--- main.py
+++ main.py
@@ -2,21 +2,24 @@
 
 # Prompt:
 # - Write a function called add_list that accepts any quantity of numbers as arguments, adds them together and returns the resulting sum.
-# - Assume all parameters will be numbers.
 # - If called with no arguments, return 0 (zero).
+# - If any non-number arguments are in the argument, return "NaN"
+
+
 # Examples:
 # add(1) //=> 1
 # add(1,50,1.23) //=> 52.23
 # add(7,-12) //=> -5
 
-
+#-----------------------------------------------
 # Solution Goes Here - >
+#-----------------------------------------------
 
 
 
 
+# Challenge: 02 remove_ends
 
-# Challenge: 02-remove_ends
 # Prompt:
 # - Write a function called remove_ends that accepts a single string argument, then returns the a string with the first and last characters removed.
 # - If the length of the string argument is less than 3, return an empty string.
@@ -25,9 +28,9 @@
 # remove_ends('Led Zeppelin Rules'); //=> "ed Zeppelin Rule"
 # remove_ends('a'); //=> "" (empty string)
 
-
+#-----------------------------------------------
 # Solution Goes Here - >
-
+#-----------------------------------------------
 
 
 
@@ -44,16 +47,16 @@
 # is_palindrome('A nut for a jar of tuna'); //=> true
 # is_palindrome(''); //=> true
 
-
+#-----------------------------------------------
 # Solution Goes Here - >
+#-----------------------------------------------
 
 
 
-
-# Challenge: 04-is_prime
+# Challenge: 04 is_prime
 
 # Prompt:
-# - Write a function named isPrime that returns true when the integer argument passed to it is a prime number and false when the argument passed to it is not prime.
+# - Write a function named is_prime that returns true when the integer argument passed to it is a prime number and false when the argument passed to it is not prime.
 # - A prime number is a whole number (integer) greater than 1 that is evenly divisible by only itself.
 # Examples:
 # is_prime(2) //=> true
@@ -62,4 +65,21 @@
 # is_prime(29) //=> true
 # is_prime(200) //=> false
 
+#-----------------------------------------------
 # Solution goes here ->
+#-----------------------------------------------
+
+
+
+# Challenge: 05 fizz_buzz
+
+# Write a program that prints the numbers from 1 to 50. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”
+# Examples:
+# fizz_buzz(10) //=> "Buzz"
+# fizz_buzz(30) //=> "FizzBuzz"
+# fizz_buzz(18) //=> "Fizz"
+# fizz_buzz(22) //=> ""
+
+#-----------------------------------------------
+# Solution Goes Here ->
+#-----------------------------------------------
