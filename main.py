38f32173ce7f73b8ed591f0b0418d7d8c83f19ec
jataubman <jeremy.taubman@generalassemb.ly>
2022-08-27 11:35:51 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# Challenge: 01 add_List
+
+# Prompt:
+# - Write a function called add_list that accepts any quantity of numbers as arguments, adds them together and returns the resulting sum.
+# - Assume all parameters will be numbers.
+# - If called with no arguments, return 0 (zero).
+# Examples:
+# add(1) //=> 1
+# add(1,50,1.23) //=> 52.23
+# add(7,-12) //=> -5
+
+
+# Solution Goes Here - >
+
+
+
+
+
+# Challenge: 02-remove_ends
+# Prompt:
+# - Write a function called remove_ends that accepts a single string argument, then returns the a string with the first and last characters removed.
+# - If the length of the string argument is less than 3, return an empty string.
+# Examples:
+# remove_ends('SEI Rocks!'); //=> "EI Rocks"
+# remove_ends('a'); //=> "" (empty string)
+
+
+# Solution Goes Here - >
